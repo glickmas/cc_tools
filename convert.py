fdeaea68b9275dc280e7ce223a217ae0ebc3625c
glickmas <glickmas@gmail.com>
2016-02-24 23:37:34 -0500

    changes filenames to read-json in and export out-dat

--- convert.py
+++ convert.py
@@ -2,8 +2,8 @@ import cc_dat_utils
 import cc_json_utils
 
 
-json_data = cc_json_utils.make_cc_data_from_json("data/dculyba_testData.json")
-cc_dat_utils.write_cc_data_to_dat(json_data, "data/dculyba_testData.dat")
-dat_data = cc_dat_utils.make_cc_data_from_dat("data/dculyba_testData.dat")
+json_data = cc_json_utils.make_cc_data_from_json("data/sglickma_testData.json")
+cc_dat_utils.write_cc_data_to_dat(json_data, "data/sglickma_testData.dat")
+dat_data = cc_dat_utils.make_cc_data_from_dat("data/sglickma_testData.dat")
 print(dat_data)
 
